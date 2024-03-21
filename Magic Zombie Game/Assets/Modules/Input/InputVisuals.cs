2a68e42f67bc8611d4a20d9a55ad58de9dab2582
MadMoth125 <madmoth125@gmail.com>
2024-03-20 23:19:13 -0500

    full commit of input assets

---
+++ Magic Zombie Game/Assets/Modules/Input/InputVisuals.cs
@@ -0,0 +1,29 @@
+using InputMapping;
+using UnityEngine;
+
+public class InputVisuals : MonoBehaviour
+{
+	public RectTransform moveableObject;
+	
+	public InputScriptableObject inputAsset;
+	
+	#region Unity Methods
+
+	private void Update()
+	{
+		if (moveableObject == null || inputAsset == null) return;
+		moveableObject.position = inputAsset.GetAbsoluteCursorPosition();
+	}
+
+	private void OnEnable()
+	{
+		inputAsset.EnableInput();
+	}
+
+	private void OnDisable()
+	{
+		inputAsset.DisableInput();
+	}
+
+	#endregion
+}
