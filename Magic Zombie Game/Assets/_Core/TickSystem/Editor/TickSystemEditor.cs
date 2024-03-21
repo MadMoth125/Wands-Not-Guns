2471b1d20838f73cb8887a7e8bf4673dfd48cbf0
MadMoth125 <madmoth125@gmail.com>
2024-03-21 03:59:30 -0500

    Update TickSystemEditor.cs

--- Magic Zombie Game/Assets/_Core/TickSystem/Editor/TickSystemEditor.cs
+++ Magic Zombie Game/Assets/_Core/TickSystem/Editor/TickSystemEditor.cs
@@ -1,11 +1,14 @@
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
+using UnityEngine;
 
 namespace Core.TickSystem.Editor
 {
 	public class TickSystemEditor : OdinMenuEditorWindow
 	{
-		private const string ASSET_PATH = "Assets/_Core/TickSystem/Resources/TickSystemBackend.asset";
+		private const string ASSET_NAME = "TickSystemBackend";
+		private const string ASSET_PATH = "Assets/_Core/TickSystem/Resources/";
+		private const string ASSET_SUFFIX = ".asset";
 		
 		[MenuItem("Window/Tick System")]
 		public static void OpenWindow()
@@ -16,7 +19,7 @@ namespace Core.TickSystem.Editor
 		protected override OdinMenuTree BuildMenuTree()
 		{
 			var tree = new OdinMenuTree();
-			tree.AddAssetAtPath("Tick System", ASSET_PATH);
+			tree.AddAssetAtPath("Tick System", ASSET_PATH + ASSET_NAME + ASSET_SUFFIX);
 			return tree;
 		}
 	}
