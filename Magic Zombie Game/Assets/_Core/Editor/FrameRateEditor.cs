9ca2d13b85726da9afea4218050e4170e4dcd27d
MadMoth125 <madmoth125@gmail.com>
2024-03-31 11:35:42 -0500

    Update FrameRateEditor.cs
    updated structure

--- Magic Zombie Game/Assets/_Core/Editor/FrameRateEditor.cs
+++ Magic Zombie Game/Assets/_Core/Editor/FrameRateEditor.cs
@@ -2,11 +2,16 @@ using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Core.Utils.Editor
 {
 	public class FrameRateEditor : OdinEditorWindow
 	{
+		private static int globalMaxFrameRate = 60;
+
+		private static string globalFrameRateDisplay = "Uncapped";
+
 		[HorizontalGroup("Properties", Width = 0.8f)]
 		[VerticalGroup("Properties/Variables")]
 		[Range(1, 240)]
@@ -20,16 +25,41 @@ namespace Core.Utils.Editor
 		[LabelWidth(80)]
 		[LabelText("Current FPS:")]
 		[DisableIf("@true")]
-		[ShowInInspector]
 		[DisplayAsString]
-		private string _currentFrameRate = "Uncapped";
+		[SerializeField]
+		private string frameRateDisplay = "Uncapped";
+
+		private const string UNCAPPED_DISPLAY_NAME = "Uncapped";
 
 		#region Static Methods
 
 		[MenuItem("Window/Custom Editors/Frame Rate Adjuster")]
 		public static void OpenWindow() => GetWindow<FrameRateEditor>().Show();
-	
-		private static void SetFrameRate(int frameRate) => Application.targetFrameRate = frameRate;
+
+		// [InitializeOnLoadMethod]
+		private static void SetFrameRateAuto()
+		{
+			SetFrameRate(globalMaxFrameRate);
+			globalFrameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
+		}
+
+		private static void SetFrameRate(int frameRate)
+		{
+			globalMaxFrameRate = frameRate;
+			Application.targetFrameRate = frameRate;
+			globalFrameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
+		}
+
+		private static string GetFrameRateDisplay(int frameRate)
+		{
+			return frameRate == -1 ? UNCAPPED_DISPLAY_NAME : frameRate.ToString();
+		}
+
+		private static void ResetFrameRate()
+		{
+			SetFrameRate(-1);
+			globalFrameRateDisplay = GetFrameRateDisplay(-1);
+		}
 
 		#endregion
 
@@ -37,21 +67,29 @@ namespace Core.Utils.Editor
 		[VerticalGroup("Properties/Buttons")]
 		[Tooltip("'Apply' sets the application's frame rate to the value above.")]
 		[Button("Apply", ButtonSizes.Medium)]
-		public void ApplyFrameRate()
+		public void Apply()
 		{
-			Application.targetFrameRate = maxFrameRate;
-			_currentFrameRate = Application.targetFrameRate == -1 ? "Uncapped" : Application.targetFrameRate.ToString();
+			SetFrameRate(maxFrameRate);
+			frameRateDisplay = GetFrameRateDisplay(maxFrameRate);
 		}
 
 		[HorizontalGroup("Properties")]
 		[VerticalGroup("Properties/Buttons")]
 		[Tooltip("'Reset' sets the application's frame rate of -1, essentially un-capping the frame rate.")]
 		[Button("Reset", ButtonSizes.Medium)]
-		public void ResetFrameRate()
+		public void Reset()
 		{
-			maxFrameRate = -1;
-			ApplyFrameRate();
+			ResetFrameRate();
 			maxFrameRate = 60;
+			frameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
+		}
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			
+			maxFrameRate = globalMaxFrameRate;
+			frameRateDisplay = globalFrameRateDisplay;
 		}
 	}
 }
