2df73bc891936ff4f87f52ff5f71a9707af9a78e
MadMoth125 <madmoth125@gmail.com>
2024-04-01 22:03:43 -0500

    Update FrameRateEditor.cs

--- Magic Zombie Game/Assets/_Core/Editor/FrameRateEditor.cs
+++ Magic Zombie Game/Assets/_Core/Editor/FrameRateEditor.cs
@@ -8,13 +8,13 @@ namespace Core.Utils.Editor
 {
 	public class FrameRateEditor : OdinEditorWindow
 	{
-		private static int globalMaxFrameRate = 60;
+		/*private static int globalMaxFrameRate = 60;
 
-		private static string globalFrameRateDisplay = "Uncapped";
+		private static string globalFrameRateDisplay = "Uncapped";*/
 
 		[HorizontalGroup("Properties", Width = 0.8f)]
 		[VerticalGroup("Properties/Variables")]
-		[Range(1, 240)]
+		[Range(-1, 240)]
 		[LabelWidth(80)]
 		[LabelText("Target FPS:")]
 		[SerializeField]
@@ -36,29 +36,14 @@ namespace Core.Utils.Editor
 		[MenuItem("Window/Custom Editors/Frame Rate Adjuster")]
 		public static void OpenWindow() => GetWindow<FrameRateEditor>().Show();
 
-		// [InitializeOnLoadMethod]
-		private static void SetFrameRateAuto()
-		{
-			SetFrameRate(globalMaxFrameRate);
-			globalFrameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
-		}
-
 		private static void SetFrameRate(int frameRate)
 		{
-			globalMaxFrameRate = frameRate;
 			Application.targetFrameRate = frameRate;
-			globalFrameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
 		}
 
 		private static string GetFrameRateDisplay(int frameRate)
 		{
 			return frameRate == -1 ? UNCAPPED_DISPLAY_NAME : frameRate.ToString();
-		}
-
-		private static void ResetFrameRate()
-		{
-			SetFrameRate(-1);
-			globalFrameRateDisplay = GetFrameRateDisplay(-1);
 		}
 
 		#endregion
@@ -79,17 +64,21 @@ namespace Core.Utils.Editor
 		[Button("Reset", ButtonSizes.Medium)]
 		public void Reset()
 		{
-			ResetFrameRate();
-			maxFrameRate = 60;
-			frameRateDisplay = GetFrameRateDisplay(globalMaxFrameRate);
+			maxFrameRate = -1;
+			SetFrameRate(maxFrameRate);
+			frameRateDisplay = GetFrameRateDisplay(maxFrameRate);
 		}
+
+		#region Unity Methods
 
 		protected override void OnEnable()
 		{
 			base.OnEnable();
 			
-			maxFrameRate = globalMaxFrameRate;
-			frameRateDisplay = globalFrameRateDisplay;
+			maxFrameRate = 60;
+			frameRateDisplay = UNCAPPED_DISPLAY_NAME;
 		}
+
+		#endregion
 	}
 }
