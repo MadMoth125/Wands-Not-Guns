158ea4e7487847b91f0107cc55b3b07b50eb078b
MadMoth125 <madmoth125@gmail.com>
2024-04-02 12:59:49 -0500

    Update EnemyTargetManager.cs
    made job start and cleanup more separated and got adjustable frame delays working

--- Magic Zombie Game/Assets/Modules/Enemy/Jobs/EnemyTargetManager.cs
+++ Magic Zombie Game/Assets/Modules/Enemy/Jobs/EnemyTargetManager.cs
@@ -17,6 +17,8 @@ namespace Enemy.Jobs
 		[SerializeField]
 		private PlayerRegistryAsset playerRegistry;
 	
+		[Tooltip("The minimum number of enemies to process in a single batch.\n" +
+		         "Higher values may improve* performance, but may cause frame rate drops.")]
 		[DisableInPlayMode]
 		[SerializeField]
 		private int minBatchCount = 10;
@@ -26,7 +28,7 @@ namespace Enemy.Jobs
 		[DisableInPlayMode]
 		[PropertyRange(1, 4)]
 		[SerializeField]
-		private int resultWaitTime = 4; // fix this not working with values under 4
+		private int resultWaitTime = 4;
 		
 		private NativeArray<Vector3> _enemyPositions;
 		private NativeArray<PlayerData> _playerData;
@@ -35,51 +37,38 @@ namespace Enemy.Jobs
 	
 		private EnemyComponent[] _cachedEnemies;
 		private (int[] id, Transform[] body) _cachedPlayers;
-	
+		
 		private int _frameCount = 0;
+		private bool _firstIfExecuted;
 	
 		#region Unity Methods
 
 		private void Update()
 		{
-			int frameMod = _frameCount % 4;
+			// job loop is handled over multiple frames to ensure the most performant execution.
+			int frameMod = _frameCount % resultWaitTime;
 		
-			if (frameMod == 0)
+			if (frameMod == 0 && !_firstIfExecuted)
 			{
-				CacheRegistryValues();
-			
-				if (enemyRegistry.Count == 0 || playerRegistry.Count == 0) return;
-
-				InitializeNativeArrays();
-		
-				FindClosestPlayerJob job = new FindClosestPlayerJob()
-				{
-					enemyPositions = _enemyPositions,
-					playerData = _playerData,
-					resultData = _resultData,
-				};
-			
-				_runningJobHandle = job.ScheduleParallel(_cachedEnemies.Length, minBatchCount, new JobHandle());
+				HandleJobBegin();
+				_firstIfExecuted = true;
 			}
-			else if (frameMod == 4 - 1)
+
+			if (_firstIfExecuted && frameMod == resultWaitTime - 1)
 			{
-				_runningJobHandle.Complete();
-				
-				for (int i = 0; i < _cachedEnemies.Length; i++)
-				{
-					var target = playerRegistry.GetValue(_resultData[i].ID);
-					if (target == null || !target.gameObject.activeSelf) continue;
-					_cachedEnemies[i].PathfindingComponent.SetTargetPosition(target.position);
-				}
-			
-				if (!_runningJobHandle.IsCompleted) return;
-			
-				DisposeNativeArrays();
+				HandleJobEnd();
+				_firstIfExecuted = false;
 			}
 		
 			_frameCount++;
 		}
 
+		private void OnEnable()
+		{
+			_frameCount = 0;
+			_firstIfExecuted = false;
+		}
+		
 		private void OnDisable()
 		{
 			_runningJobHandle.Complete();
@@ -90,12 +79,62 @@ namespace Enemy.Jobs
 
 		#endregion
 
+		/// <summary>
+		/// Handles the initialization of the job and the subsequent scheduling of the job.
+		/// </summary>
+		private void HandleJobBegin()
+		{
+			CacheRegistryValues();
+			
+			if (enemyRegistry.Count == 0 || playerRegistry.Count == 0) return;
+
+			InitializeNativeArrays();
+		
+			FindClosestPlayerJob job = new FindClosestPlayerJob()
+			{
+				enemyPositions = _enemyPositions,
+				playerData = _playerData,
+				resultData = _resultData,
+			};
+			
+			_runningJobHandle = job.ScheduleParallel(_cachedEnemies.Length, minBatchCount, new JobHandle());
+		}
+
+		/// <summary>
+		/// Handles the completion of the job and the subsequent processing of the job results.
+		/// </summary>
+		private void HandleJobEnd()
+		{
+			_runningJobHandle.Complete();
+
+			if (playerRegistry != null && playerRegistry.Count != 0 && _resultData.Length != 0)
+			{
+				for (int i = 0; i < _cachedEnemies.Length; i++)
+				{
+					var target = playerRegistry.GetValue(_resultData[i].ID);
+					if (target == null || !target.gameObject.activeSelf) continue;
+					_cachedEnemies[i].PathfindingComponent.SetTargetPosition(target.position);
+				}
+			}
+			
+			if (!_runningJobHandle.IsCompleted) return;
+			
+			DisposeNativeArrays();
+		}
+		
+		/// <summary>
+		/// Stores the current values of the enemy and player registries in external arrays.
+		/// Useful to prevent constant registry lookups/accesses during the job.
+		/// </summary>
 		private void CacheRegistryValues()
 		{
 			_cachedEnemies = enemyRegistry.GetValuesArray();
 			_cachedPlayers = (playerRegistry.GetKeysArray(), playerRegistry.GetValuesArray());
 		}
 		
+		/// <summary>
+		/// Converts the cached data into native arrays for use in the job.
+		/// </summary>
 		private void InitializeNativeArrays()
 		{
 			_enemyPositions = new NativeArray<Vector3>(_cachedEnemies.Length, Allocator.TempJob);
@@ -114,6 +153,10 @@ namespace Enemy.Jobs
 			}
 		}
 		
+		/// <summary>
+		/// Disposes of the native arrays used in the job.
+		/// Only disposes if the arrays have been created.
+		/// </summary>
 		private void DisposeNativeArrays()
 		{
 			if (_enemyPositions.IsCreated)
