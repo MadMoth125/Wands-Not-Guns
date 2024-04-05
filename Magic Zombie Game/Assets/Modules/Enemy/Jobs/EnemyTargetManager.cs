cc47a73ea8150ecea8995d44da9f9bc922ae2713
MadMoth125 <madmoth125@gmail.com>
2024-04-04 19:57:33 -0500

    fixed faulty logic code and update methods
    fixed job struct looping over more elements than needed, and moved job scheduling to use the custom tick system rather than Unity's update loop.
    
    Also updated inspector formatting for variables

--- Magic Zombie Game/Assets/Modules/Enemy/Jobs/EnemyTargetManager.cs
+++ Magic Zombie Game/Assets/Modules/Enemy/Jobs/EnemyTargetManager.cs
@@ -1,15 +1,21 @@
+using System;
+using System.Linq;
 using Core.CustomDebugger;
+using Core.CustomTickSystem;
 using Enemy.Registry;
 using Player.Registry;
 using Sirenix.OdinInspector;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enemy.Jobs
 {
 	public class EnemyTargetManager : MonoBehaviour
 	{
+		#region Fields
+
 		[Required]
 		[SerializeField]
 		private EnemyRegistryAsset enemyRegistry;
@@ -17,19 +23,22 @@ namespace Enemy.Jobs
 		[Required]
 		[SerializeField]
 		private PlayerRegistryAsset playerRegistry;
-	
+
+		[TitleGroup("Jobs", "Parameters", Alignment = TitleAlignments.Centered)]
 		[Tooltip("The minimum number of enemies to process in a single batch.\n" +
 		         "Higher values may improve performance for larger enemy groups.")]
 		[DisableInPlayMode]
 		[SerializeField]
-		private int minBatchCount = 10;
-	
-		[Tooltip("The number of frames to wait before completing the search job.\n" +
-		         "The allocation type used requires that the job finishes within 4 frames, so this value should be no higher than 4.")]
+		private int batchCount = 10;
+		
+		[TitleGroup("Jobs")]
+		[Tooltip("The number of frames to wait before completing the job.\n" +
+		         "Useful for giving the job more time to calculate & complete the given task.\n\n" +
+		         "The '<b>Allocator.TempJob</b>' type requires that the job finishes within 4 frames, so this value should be no higher than 4.")]
 		[DisableInPlayMode]
 		[PropertyRange(1, 4)]
 		[SerializeField]
-		private int resultWaitTime = 4;
+		private int completionWaitTime = 4;
 		
 		[TitleGroup("Debug", "Parameters", Alignment = TitleAlignments.Centered)]
 		[SerializeField]
@@ -44,61 +53,71 @@ namespace Enemy.Jobs
 		[Tooltip("Enable logging of the native arrays and their state.")]
 		[SerializeField]
 		private bool logNativeArrays = false;
+
+		#endregion
+
+		private int _frameCount = 0;
+		private bool _jobActive = false;
+		
+		private EnemyComponent[] _cachedEnemies;
+		private PlayerData[] _cachedPlayerData;
 		
 		private NativeArray<Vector3> _enemyPositions;
 		private NativeArray<PlayerData> _playerData;
 		private NativeArray<PlayerData> _resultData;
+		private FindClosestPlayerJob _findPlayerJob;
 		private JobHandle _runningJobHandle;
-	
-		private EnemyComponent[] _cachedEnemies;
-		private (int[] id, Transform[] body) _cachedPlayers;
-		
-		private int _frameCount = 0;
-		private bool _firstIfExecuted;
-	
+		private readonly JobHandle _dependencyJobHandleHandle = new();
+
 		#region Unity Methods
 
-		private void Update()
+		private void Awake()
 		{
-			// job loop is handled over multiple frames to ensure the most performant execution.
-			int frameMod = _frameCount % resultWaitTime;
-		
-			if (frameMod == 0 && !_firstIfExecuted)
-			{
-				HandleJobBegin();
-				_firstIfExecuted = true;
-				
-				if (logJobStartAndEnd) LogWrapper("Job started at frame '{_frameCount}'.", LoggerAsset.LogType.Info);
-			}
-
-			// if the job has been started and the we've waited enough frames, end the job
-			if (_firstIfExecuted && frameMod == resultWaitTime - 1)
-			{
-				HandleJobEnd();
-				_firstIfExecuted = false;
-				
-				if (logJobStartAndEnd) LogWrapper("Job ended at frame '{_frameCount}'.", LoggerAsset.LogType.Info);
-			}
-		
-			_frameCount++;
+			_findPlayerJob = new FindClosestPlayerJob();
 		}
 
 		private void OnEnable()
 		{
 			_frameCount = 0;
-			_firstIfExecuted = false;
+			TickSystem.AddListener("EnemyTarget", OnEnemyTick);
 		}
-		
+
 		private void OnDisable()
 		{
+			TickSystem.RemoveListener("EnemyTarget", OnEnemyTick);
 			_runningJobHandle.Complete();
 			if (!_runningJobHandle.IsCompleted) return;
 		
 			DisposeNativeArrays();
 		}
 
+		private void Update()
+		{
+			if (_jobActive)
+			{
+				if (_frameCount == completionWaitTime)
+				{
+					HandleJobEnd();
+					_jobActive = false;
+					_frameCount = 0;
+					if (logJobStartAndEnd) LogWrapper($"Job ended at frame '{Time.frameCount}'.", LoggerAsset.LogType.Info);
+				}
+				else
+				{
+					_frameCount++;
+				}
+			}
+		}
+
 		#endregion
 
+		private void OnEnemyTick()
+		{
+			HandleJobBegin();
+			_jobActive = true;
+			if (logJobStartAndEnd) LogWrapper($"Job started at frame '{Time.frameCount}'.", LoggerAsset.LogType.Info);
+		}
+		
 		/// <summary>
 		/// Handles the initialization of the job and the subsequent scheduling of the job.
 		/// </summary>
@@ -107,17 +126,13 @@ namespace Enemy.Jobs
 			CacheRegistryValues();
 			
 			if (enemyRegistry.Count == 0 || playerRegistry.Count == 0) return;
-
-			InitializeNativeArrays();
-		
-			FindClosestPlayerJob job = new FindClosestPlayerJob()
-			{
-				enemyPositions = _enemyPositions,
-				playerData = _playerData,
-				resultData = _resultData,
-			};
 			
-			_runningJobHandle = job.ScheduleParallel(_cachedEnemies.Length, minBatchCount, new JobHandle());
+			InitializeNativeArrays();
+			
+			// set the job data
+			_findPlayerJob.SetData(_enemyPositions, _playerData, _resultData);
+			
+			_runningJobHandle = _findPlayerJob.ScheduleParallel(_cachedEnemies.Length, batchCount, _dependencyJobHandleHandle);
 		}
 
 		/// <summary>
@@ -153,7 +168,14 @@ namespace Enemy.Jobs
 		private void CacheRegistryValues()
 		{
 			_cachedEnemies = enemyRegistry.GetValuesArray();
-			_cachedPlayers = (playerRegistry.GetKeysArray(), playerRegistry.GetValuesArray());
+			
+			int[] playerKeys = playerRegistry.GetKeysArray();
+			Transform[] playerValues = playerRegistry.GetValuesArray();
+			_cachedPlayerData = new PlayerData[playerKeys.Length];
+			for (int i = 0; i < _cachedPlayerData.Length; i++)
+			{
+				_cachedPlayerData[i] = new PlayerData(playerKeys[i], playerValues[i].position);
+			}
 		}
 		
 		/// <summary>
@@ -162,9 +184,9 @@ namespace Enemy.Jobs
 		private void InitializeNativeArrays()
 		{
 			_enemyPositions = new NativeArray<Vector3>(_cachedEnemies.Length, Allocator.TempJob);
-			_playerData = new NativeArray<PlayerData>(_cachedPlayers.id.Length, Allocator.TempJob);
+			_playerData = new NativeArray<PlayerData>(_cachedPlayerData.Length, Allocator.TempJob);
 			_resultData = new NativeArray<PlayerData>(_cachedEnemies.Length, Allocator.TempJob);
-		
+
 			// fill the native arrays with the cached data
 			for (int i = 0; i < _cachedEnemies.Length; i++)
 			{
@@ -173,12 +195,14 @@ namespace Enemy.Jobs
 			
 			if (logNativeArrays) LogWrapper($"Native array '{nameof(_enemyPositions)}' initialized w/ {_cachedEnemies.Length} items.", LoggerAsset.LogType.Info);
 		
-			for (int i = 0; i < _cachedPlayers.id.Length; i++)
+			PlayerData reusablePlayerData = new();
+			for (int i = 0; i < _cachedPlayerData.Length; i++)
 			{
-				_playerData[i] = new PlayerData(_cachedPlayers.id[i], _cachedPlayers.body[i].position);
+				reusablePlayerData.SetData(_cachedPlayerData[i].ID, _cachedPlayerData[i].Position);
+				_playerData[i] = reusablePlayerData;
 			}
 			
-			if (logNativeArrays) LogWrapper($"Native array '{nameof(_playerData)}' initialized w/ {_cachedPlayers.id.Length} items.", LoggerAsset.LogType.Info);
+			if (logNativeArrays) LogWrapper($"Native array '{nameof(_playerData)}' initialized w/ {_cachedPlayerData.Length} items.", LoggerAsset.LogType.Info);
 		}
 		
 		/// <summary>
@@ -208,14 +232,10 @@ namespace Enemy.Jobs
 		
 		private void LogWrapper(string message, LoggerAsset.LogType type)
 		{
-			#if UNITY_EDITOR
-			
 			if (logger != null)
 			{
 				logger.Log(message, this, type);
 			}
-			
-			#endif
 		}
 	}
 }
--- Magic Zombie Game/Assets/Modules/Enemy/Jobs/FindClosestPlayerJob.cs
+++ Magic Zombie Game/Assets/Modules/Enemy/Jobs/FindClosestPlayerJob.cs
@@ -19,30 +19,59 @@ namespace Enemy.Jobs
 		/// The positions of all currently active enemies.
 		/// </summary>
 		[ReadOnly]
-		public NativeArray<Vector3> enemyPositions;
+		private NativeArray<Vector3> _enemyPositions;
 	
 		/// <summary>
 		/// The IDs/positions of all currently active players.
 		/// </summary>
 		[ReadOnly]
-		public NativeArray<PlayerData> playerData;
+		private NativeArray<PlayerData> _playerData;
 	
 		/// <summary>
 		/// The resulting IDs/positions of the closest player to each enemy.
 		/// </summary>
 		public NativeArray<PlayerData> resultData;
-	
+
+		#if true
+		
+		// fixed code: now we only loop through each player and compare it against the current index enemy.
 		public void Execute(int index)
 		{
 			// save outside of loop for more re-usability
 			PlayerData bestPlayer = new PlayerData(-1, Vector3.zero);
 
-			foreach (var enemyPos in enemyPositions)
+			bestPlayer.SetData(-1, Vector3.zero);
+			float bestDistance = float.MaxValue;
+
+			foreach (var dat in _playerData)
+			{
+				Vector3 distVec = dat.Position - _enemyPositions[index];
+				float dist = distVec.sqrMagnitude;
+				
+				if (dist < bestDistance)
+				{
+					bestPlayer = dat;
+					bestDistance = dist;
+				}
+			}
+			
+			resultData[index] = bestPlayer;
+		}
+		
+		#else
+		
+		// problematic code: the outer loop is not necessary because the job already has an iteration index.
+		public void Execute(int index)
+		{
+			// save outside of loop for more re-usability
+			PlayerData bestPlayer = new PlayerData(-1, Vector3.zero);
+
+			foreach (var enemyPos in _enemyPositions)
 			{
 				bestPlayer.SetData(-1, Vector3.zero);
 				float bestDistance = float.MaxValue;
 
-				foreach (var dat in playerData)
+				foreach (var dat in _playerData)
 				{
 					Vector3 distVec = dat.Position - enemyPos;
 					float dist = distVec.sqrMagnitude;
@@ -56,6 +85,15 @@ namespace Enemy.Jobs
 			
 				resultData[index] = bestPlayer;
 			}
+		}
+		
+		#endif
+		
+		public void SetData(in NativeArray<Vector3> enemyPositionArray, in NativeArray<PlayerData> playerDataArray, in NativeArray<PlayerData> resultArray)
+		{
+			_enemyPositions = enemyPositionArray;
+			_playerData = playerDataArray;
+			this.resultData = resultArray;
 		}
 	}
 }
