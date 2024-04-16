b72ecc8f5f8fe95bf35d54097724c2c7eef9365d
MadMoth125 <madmoth125@gmail.com>
2024-04-15 21:36:53 -0500

    added logger, updated layout, and removed info box from EnemyHealthSetter.cs

--- Magic Zombie Game/Assets/Modules/Game/Managers/EnemyManagerComponents/EnemyHealthSetter.cs
+++ Magic Zombie Game/Assets/Modules/Game/Managers/EnemyManagerComponents/EnemyHealthSetter.cs
@@ -1,12 +1,13 @@
+using Core.CustomDebugger;
 using Enemy.Registry;
 using Obvious.Soap;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-[TypeInfoBox("This component is responsible for setting the health of an enemy when it is spawned.\n" +
+/*[TypeInfoBox("This component is responsible for setting the health of an enemy when it is spawned.\n" +
              "It listens for the onEnemySpawnedEvent and sets the max health of the enemy to the value from the RoundHealthScriptableObject.\n" +
-             "The RoundHealthScriptableObject already has the logic and references to the RoundNumberScriptableObject to calculate the max health of the enemy.")]
+             "The RoundHealthScriptableObject already has the logic and references to the RoundNumberScriptableObject to calculate the max health of the enemy.")]*/
 [DisallowMultipleComponent]
 public class EnemyHealthSetter : MonoBehaviour, IManagerComponent<EnemyManager>
 {
@@ -20,9 +21,20 @@ public class EnemyHealthSetter : MonoBehaviour, IManagerComponent<EnemyManager>
 	[SerializeField]
 	private EnemyRegistry enemyRegistry;
 
+	[Space(10)]
+	
+	[TabGroup("Scriptable Objects", Icon = SdfIconType.Box)]
 	[Required]
 	[SerializeField]
 	private RoundHealthScriptableObject roundHealth;
+	
+	[TabGroup("Debug Settings", Icon = SdfIconType.Bug)]
+	[SerializeField]
+	private LoggerScriptableObject logger;
+	
+	[TabGroup("Debug Settings")]
+	[SerializeField]
+	private bool logHealthSet = false;
 
 	private EnemyManager _manager;
 	
@@ -64,6 +76,16 @@ public class EnemyHealthSetter : MonoBehaviour, IManagerComponent<EnemyManager>
 		if (enemy != null)
 		{
 			enemy.HealthComponent.SetMaxHealth(roundHealth.GetMaxHealth(), true);
+			LogWrapper($"Enemy '{enemyId}' health is '{roundHealth.GetMaxHealth()}'.", LoggerType.Info);
 		}
+	}
+	
+	private void LogWrapper(string message, LoggerType type)
+	{
+		if (logger != null)
+		{
+			logger.Log(message, this, type);
+		}
+		
 	}
 }
