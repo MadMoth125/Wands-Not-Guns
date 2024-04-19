2bc37dec1d9657cb4c48575e4a6c073475e368a8
MadMoth125 <madmoth125@gmail.com>
2024-04-19 13:16:12 -0500

    Update ShootComponent.cs

--- Magic Zombie Game/Assets/Modules/BetterWeapons/ShootComponents/ShootComponent.cs
+++ Magic Zombie Game/Assets/Modules/BetterWeapons/ShootComponents/ShootComponent.cs
@@ -1,51 +1,66 @@
 using System;
+using Core.Owning;
 using Mr_sB.UnityTimer;
 using UnityEngine;
 
-[DisallowMultipleComponent]
-public class ShootComponent : MonoBehaviour
+namespace Weapons
 {
-	public event Action OnShoot;
+	[DisallowMultipleComponent]
+	public class ShootComponent : MonoBehaviour, IOwnable<GunComponent>
+	{
+		public event Action OnShoot;
 	
-	[SerializeField]
-	private int fireRate = 5;
+		[SerializeField]
+		private int fireRate = 5;
 
-	[SerializeField]
-	private bool isAutomatic = true;
+		[SerializeField]
+		private bool isAutomatic = true;
 	
-	private FullAutoShootComponent _fullAutoShootComponent;
-	private SemiAutoShootComponent _semiAutoShootComponent;
-	
-	public void StartShoot()
-	{
-		if (isAutomatic) _fullAutoShootComponent.BeginFire();
-		else _semiAutoShootComponent.BeginFire();
-	}
-	
-	public void StopShoot()
-	{
-		if (isAutomatic) _fullAutoShootComponent.EndFire();
-		// SemiAuto doesn't need to be stopped
-	}
-	
-	#region Unity Methods
+		private GunComponent _owner;
+		private FullAutoShootComponent _fullAutoShootComponent;
+		private SemiAutoShootComponent _semiAutoShootComponent;
 
-	private void Awake()
-	{
-		_fullAutoShootComponent = new FullAutoShootComponent(1f / fireRate, HandleFire);
-		_semiAutoShootComponent = new SemiAutoShootComponent(1f / fireRate, HandleFire);
-	}
+		public void StartShoot()
+		{
+			if (isAutomatic) _fullAutoShootComponent.BeginFire();
+			else _semiAutoShootComponent.BeginFire();
+		}
 
-	private void OnDestroy()
-	{
-		_fullAutoShootComponent?.Dispose();
-		_semiAutoShootComponent?.Dispose();
-	}
+		public void StopShoot()
+		{
+			if (isAutomatic) _fullAutoShootComponent.EndFire();
+			// SemiAuto doesn't need to be stopped
+		}
 
-	#endregion
-	
-	private void HandleFire()
-	{
-		OnShoot?.Invoke();
+		public GunComponent GetOwner()
+		{
+			return _owner;
+		}
+		
+		public void SetOwner(GunComponent owner)
+		{
+			_owner = owner;
+		}
+
+		#region Unity Methods
+
+		private void Awake()
+		{
+			_fullAutoShootComponent = new FullAutoShootComponent(1f / fireRate, HandleFire);
+			_semiAutoShootComponent = new SemiAutoShootComponent(1f / fireRate, HandleFire);
+		}
+
+		private void OnDestroy()
+		{
+			_fullAutoShootComponent?.Dispose();
+			_semiAutoShootComponent?.Dispose();
+		}
+
+		#endregion
+
+		private void HandleFire()
+		{
+			OnShoot?.Invoke();
+		}
 	}
 }
