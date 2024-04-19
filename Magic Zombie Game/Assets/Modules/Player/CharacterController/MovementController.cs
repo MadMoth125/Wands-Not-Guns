3d23bc4a598fcd5f8d3c95f6d73082919b210d22
MadMoth125 <madmoth125@gmail.com>
2024-04-19 13:18:22 -0500

    Update MovementController.cs
    added exposed movement components and added impulse function for knockback against player.

--- Magic Zombie Game/Assets/Modules/Player/CharacterController/MovementController.cs
+++ Magic Zombie Game/Assets/Modules/Player/CharacterController/MovementController.cs
@@ -14,20 +14,21 @@ namespace Player.Controller
 	{
 		#region Properties
 
+		public CharacterRotation RotationComponent => rotationComponent;
+		public CharacterMovement MovementComponent => movementComponent;
+		public GravityMovement GravityComponent => gravityComponent;
+		
 		public CharacterGroundingReport GroundedStatus => _motor != null ? _motor.GroundingStatus : new CharacterGroundingReport();
-
 		public Vector3 Velocity => _motor != null ? _motor.Velocity : Vector3.zero;
-	
 		public Vector3 BaseVelocity => _motor != null ? _motor.BaseVelocity : Vector3.zero;
-	
 		public Vector3 CharacterUp => _motor != null ? _motor.CharacterUp : Vector3.up;
-	
 		public Vector3 CharacterForward => _motor != null ? _motor.CharacterForward : Vector3.forward;
-	
 		public Vector3 CharacterRight => _motor != null ? _motor.CharacterRight : Vector3.right;
 
 		#endregion
-	
+
+		#region Fields
+
 		[SerializeField]
 		private ScriptableObjectGameControls gameControls;
 	
@@ -37,6 +38,11 @@ namespace Player.Controller
 		private CharacterRotation rotationComponent = new();
 		
 		[Title("Movement")]
+		
+		[Tooltip("Enable or disable the ability to apply impulse forces to the character motor.")]
+		[SerializeField]
+		private bool enableImpulse = true;
+		
 		[Inlined]
 		[SerializeField]
 		private CharacterMovement movementComponent = new();
@@ -49,10 +55,25 @@ namespace Player.Controller
 		[Title("Collision")]
 		[SerializeField]
 		private LayerMaskVariable collisionLayerMask;
+
+		#endregion
 		
 		private IEnumerable<IMovementAbility> _abilityCollection;
 		private KinematicCharacterMotor _motor;
+		private Vector3 _impact;
+		
 
+		/// <summary>
+		/// Applies an impulse force to the character motor.
+		/// </summary>
+		/// <param name="direction">The direction of the impulse force.</param>
+		/// <param name="force">The strength of the impulse force.</param>
+		public void ApplyImpulse(Vector3 direction, float force)
+		{
+			if (!enableImpulse) return;
+			_impact = direction.normalized * force / _motor.SimulatedCharacterMass;
+		}
+		
 		#region Unity Methods
 
 		private void Awake()
@@ -95,7 +116,7 @@ namespace Player.Controller
 
 		#endregion
 
-		public void UpdateRotation(ref Quaternion currentRotation, float deltaTime)
+		void ICharacterController.UpdateRotation(ref Quaternion currentRotation, float deltaTime)
 		{
 			foreach (var ability in _abilityCollection)
 			{
@@ -103,29 +124,18 @@ namespace Player.Controller
 			}
 		}
 
-		public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
+		void ICharacterController.UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
 		{
+			if (_impact.sqrMagnitude > 0.2f)
+			{
+				currentVelocity += _impact * deltaTime;
+				_impact = Vector3.Lerp(_impact, Vector3.zero, deltaTime * 10f);
+			}
+			
 			foreach (var ability in _abilityCollection)
 			{
 				ability.UpdateVelocity(ref currentVelocity, deltaTime);
 			}
-		}
-
-		#region Misc ICharacterController Methods
-
-		public void BeforeCharacterUpdate(float deltaTime)
-		{
-		
-		}
-
-		public void PostGroundingUpdate(float deltaTime)
-		{
-		
-		}
-
-		public void AfterCharacterUpdate(float deltaTime)
-		{
-		
 		}
 
 		public bool IsColliderValidForCollisions(Collider coll)
@@ -133,23 +143,40 @@ namespace Player.Controller
 			return collisionLayerMask != null && (collisionLayerMask.Value & (1 << coll.gameObject.layer)) != 0;
 		}
 
-		public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		#region Misc ICharacterController Methods
+
+		void ICharacterController.BeforeCharacterUpdate(float deltaTime)
 		{
 		
 		}
 
-		public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		void ICharacterController.PostGroundingUpdate(float deltaTime)
 		{
 		
 		}
 
-		public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 atCharacterPosition,
+		void ICharacterController.AfterCharacterUpdate(float deltaTime)
+		{
+		
+		}
+
+		void ICharacterController.OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		{
+		
+		}
+
+		void ICharacterController.OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		{
+		
+		}
+
+		void ICharacterController.ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 atCharacterPosition,
 			Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport)
 		{
 		
 		}
 
-		public void OnDiscreteCollisionDetected(Collider hitCollider)
+		void ICharacterController.OnDiscreteCollisionDetected(Collider hitCollider)
 		{
 		
 		}
